4324337b5e98cb24f88dd9520a393805a1dd4d9c
Todd W Crone <twcrone@gmail.com>
2021-06-16 15:55:39 -0400

    Make run script more generic

--- run-with-newrelic.sh
+++ run-with-newrelic.sh
@@ -1,1 +1,3 @@
-java -Dnewrelic.environment=$NEWRELIC_ENV -javaagent:$NEWRELIC_HOME/newrelic.jar -jar build/libs/dynamodb-example-0.0.1-SNAPSHOT.jar
+# NEWRELIC_ENV is directory where newrelic.yml you want to use resides (e.g. "/path/to/yml/directory")
+# NEWRELIC_JAR is full path to the newrelic.jar (e.g. "/path/to/file.jar")
+java -Dnewrelic.environment=$NEWRELIC_ENV -javaagent:$NEWRELIC_JAR -jar build/libs/*-SNAPSHOT.jar
